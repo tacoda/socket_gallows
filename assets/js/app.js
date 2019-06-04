59ca0dbf7b8596f3508776710af0634e65c7ba48
Ian Johnson <ijohnson@covermymeds.com>
2019-06-04 12:42:29 -0400

    Wrap up into an SPA using Vue

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,22 +26,16 @@ import "phoenix_html"
 
 import {Socket} from "phoenix"
 
-class Hangman {
+class HangmanServer {
 
-  constructor() {
+  constructor(tally) {
+    this.tally = tally
     this.socket = new Socket("/socket", {params: {token: window.userToken}})
     this.socket.connect()
   }
 
   connect_to_hangman() {
     this.setup_channel()
-    this.channel.on("tally", tally => {
-      console.log(tally)
-    })
-  }
-
-  setup_channel() {
-    this.channel = this.socket.channel("hangman:game", {})
     this.channel
         .join()
         .receive("ok", resp => {
@@ -54,10 +48,93 @@ class Hangman {
         })
   }
 
+  setup_channel() {
+    this.channel = this.socket.channel("hangman:game", {})
+    this.channel.on("tally", tally => {
+      this.copy_tally(tally)
+    })
+  }
+
   fetch_tally() {
     this.channel.push("tally", {})
   }
+
+  make_move(guess) {
+    this.channel.push("make_move", guess)
+  }
+
+  new_game() {
+    this.channel.push("new_game", {})
+  }
+
+  copy_tally(from) {
+    for (let k in from) {
+      this.tally[k] = from[k]
+    }
+  }
 }
 
-let app = new Hangman()
-app.connect_to_hangman()
+const RESPONSES = {
+  won:          [ "success", "You Won!" ],
+  lost:         [ "danger", "You Lost!" ],
+  good_guess:   [ "success", "Good guess!" ],
+  bad_guess:    [ "warning", "Bad guess!" ],
+  already_used: [ "info", "You already guessed that" ],
+  initializing: [ "info", "Let's Play!"]
+}
+
+let Vue = require("vue/dist/vue.min.js")
+
+let view = function(hangman) {
+  let app = new Vue({
+    el: "#app",
+    data: {
+      tally: hangman.tally
+    },
+    computed: {
+      game_over: function() {
+        let state = this.tally.game_state
+        return (state == "won") || (state == "lost")
+      },
+      game_state_message: function() {
+        let state = this.tally.game_state
+        return RESPONSES[state][1]
+      },
+      game_state_class: function() {
+        let state = this.tally.game_state
+        return RESPONSES[state][0]
+      }
+    },
+    methods: {
+      guess: function(ch) {
+        hangman.make_move(ch)
+      },
+      new_game: function() {
+        hangman.new_game()
+      },
+      already_guessed: function(ch) {
+        return this.tally.used.indexOf(ch) >= 0
+      },
+      correct_guess: function(ch) {
+        return this.already_guessed(ch) &&
+                (this.tally.letters.indexOf(ch) >= 0)
+      },
+      turns_gt: function(left) {
+        return this.tally.turns_left > left
+      }
+    }
+  })
+  return app
+}
+
+// window.onload = function() {}
+let tally = {
+  turns_left: 7,
+  letters:    ["a", "_", "c"],
+  game_state: "initializing",
+  used:       []
+}
+
+let hangman = new HangmanServer(tally)
+let app = view(hangman)
+hangman.connect_to_hangman()
--- lib/socket_gallows_web/channels/hangman_channel.ex
+++ lib/socket_gallows_web/channels/hangman_channel.ex
@@ -2,16 +2,25 @@ defmodule SocketGallowsWeb.HangmanChannel do
   use Phoenix.Channel
 
   def join("hangman:game", _, socket) do
-    game = Hangman.new_game()
     # assign plays same role as session with traditional requests
-    socket = assign(socket, :game, game)
+    socket = socket |> assign(:game, Hangman.new_game())
     { :ok, socket }
   end
 
   def handle_in("tally", _, socket) do
-    game = socket.assigns.game
-    tally = Hangman.tally(game)
+    tally = socket.assigns.game |> Hangman.tally()
     push(socket, "tally", tally)
     { :noreply, socket}
+  end
+
+  def handle_in("make_move", guess, socket) do
+    tally = socket.assigns.game |> Hangman.make_move(guess)
+    push(socket, "tally", tally)
+    { :noreply, socket }
+  end
+
+  def handle_in("new_game", _, socket) do
+    socket = socket |> assign(:game, Hangman.new_game())
+    handle_in("tally", nil, socket)
   end
 end
